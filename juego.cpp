c647fe07f674b7bbb6cc938dbf14e768e6c52e2d
AlmuHS <liberamenso10000@gmail.com>
2018-04-09 14:42:19 +0200

    creada nueva clase

---
+++ juego.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "juego.h"
+
+juego::juego(){
+
+}
+
+int juego::gen_number(){
+
+}
+
+int juego::gen_operator(){
+
+}
+
+void juego::show_operation(){
+
+}
+
+bool juego::check_result(){
+
+}
+
+
---
+++ juego.h
@@ -0,0 +1,14 @@
+
+
+class juego{
+	int num1, num2, op;
+	char operation[4];
+
+	public:
+		juego();
+		int gen_number();
+		int gen_operator();
+	
+		void show_operation();
+		bool check_result();	
+};
