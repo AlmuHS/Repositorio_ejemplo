5b5f2461f23c284e6204ebc34f029918b7aeb164
AlmuHS <liberamenso10000@gmail.com>
2018-04-10 11:13:01 +0200

    terminado main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include "juego.h"
+#include <iostream>
+
 
 int main(){
+	juego preguntas;
+	int puntuacion = 0;
 
+	for(int i = 0; i < 10; i++){
+		preguntas.gen_number();
+		preguntas.gen_operator();
+
+		preguntas.show_operation();
+		
+		if(preguntas.check_answer()){
+			puntuacion += 5;
+		}
+		else puntuacion -= 2;
+	}
+	
+	std::cout<<"Tu puntuacion es "<<puntuacion;
+	
 	return 0;
 }
